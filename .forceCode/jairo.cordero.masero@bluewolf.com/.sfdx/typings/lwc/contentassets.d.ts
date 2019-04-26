5c13bc9dabf92d16cdb812e00db6faf976775100
irizalwow <49157339+irizalwow@users.noreply.github.com>
2019-04-26 13:59:52 +0200

    Delete contentassets.d.ts

--- .forceCode/jairo.cordero.masero@bluewolf.com/.sfdx/typings/lwc/contentassets.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@salesforce/contentAssetUrl/X439fd6e1bb89d8dcb3a3b2d201b82fe5" {
-    var X439fd6e1bb89d8dcb3a3b2d201b82fe5: string;
-    export default X439fd6e1bb89d8dcb3a3b2d201b82fe5;
-}
-declare module "@salesforce/contentAssetUrl/descarga" {
-    var descarga: string;
-    export default descarga;
-}
