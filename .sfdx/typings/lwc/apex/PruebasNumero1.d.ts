410bba4ae553beb8441e1ce9d17b49ceef54b461
irizalwow <49157339+irizalwow@users.noreply.github.com>
2019-04-26 14:02:45 +0200

    Delete PruebasNumero1.d.ts

--- .sfdx/typings/lwc/apex/PruebasNumero1.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/PruebasNumero1.prueba1" {
-  export default function prueba1(param: {apiName: any}): Promise<any>;
-}
-declare module "@salesforce/apex/PruebasNumero1.prueba2" {
-  export default function prueba2(param: {apiName: any}): Promise<any>;
-}
